54b01bb1070aed361e46c43045bb9abce06d6556
HasanAouzoun <hasanozon4@gmail.com>
2019-03-17 08:42:28 +0000

    techdebt: changed to v6 encode

--- src/server.js
+++ src/server.js
@@ -1,27 +1,27 @@
 // Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
+import express, { static } from 'express';
+import { join } from 'path';
+import { createServer } from 'http';
+import { json, urlencoded } from 'body-parser';
 
 // Get our API routes
-const api = require('./server/routes/api');
+import api from './server/routes/api';
 
 const app = express();
 
 // Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(static(join(__dirname, 'dist')));
 
 // Set our api routes
 app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(join(__dirname, 'dist/index.html'));
 });
 
 /**
@@ -33,7 +33,7 @@ app.set('port', port);
 /**
  * Create HTTP server.
  */
-const server = http.createServer(app);
+const server = createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
