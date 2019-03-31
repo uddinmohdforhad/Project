273e11d7e87513b9ddecf3d80bf8dc88a982005f
HasanAouzoun <hasanozon4@gmail.com>
2019-03-31 00:11:24 +0000

    techdebt

--- src/app/services/dashboard.service.spec.ts
+++ src/app/services/dashboard.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { DashboardService } from './dashboard.service';
 
-describe('AuthService', () => {
+describe('DashboardService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
