44a48c8a5160678e0d8e61ef92d51ade134b8025
HasanAouzoun <hasanozon4@gmail.com>
2019-04-28 20:36:02 +0100

    alert message for logging

--- src/app/website/login/login.component.ts
+++ src/app/website/login/login.component.ts
@@ -25,6 +25,7 @@ export class LoginComponent implements OnInit {
       res => {
         console.log(res),
         localStorage.setItem('token', res.token)
+        alert('You have registered successfully')
         this._router.navigate([''])
       },// redirect to home and logged in
       err => {
