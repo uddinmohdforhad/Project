06f3c6e0a640595f77d50dd439ce2f4d89818af2
HasanAouzoun <hasanozon4@gmail.com>
2019-05-05 12:20:46 +0100

    added book a table in the FE

--- src/app/website/booking/booking.component.ts
+++ src/app/website/booking/booking.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { _MatAutocompleteMixinBase } from '@angular/material';
 
 @Component({
   selector: 'booking',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking.component.css']
 })
 export class BookingComponent implements OnInit {
+  bookingData = {
+    date:"",
+    capacity: "",
+    token: ""
+  }
 
-  constructor() { }
+  constructor(private _auth: AuthService,
+              private _router: Router) { }
 
   ngOnInit() {
   }
 
+  booktable(){
+    this.bookingData.token = this._auth.getToken()
+    this._auth.book(this.bookingData)
+      .subscribe(
+        res => {
+          console.log(res)
+          alert(res.message),
+          this._router.navigate([''])
+        },
+        err => {
+          alert(`Error: ${err.error.message}`)
+        }
+      )
+  }
 }
