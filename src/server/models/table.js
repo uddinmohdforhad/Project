cda1e6bd3be4e951305765b0eebafd00595ac8c7
HasanAouzoun <hasanozon4@gmail.com>
2019-06-26 07:21:31 +0100

    table availability added

--- src/server/models/table.js
+++ src/server/models/table.js
@@ -4,14 +4,7 @@ const Schema = mongoose.Schema;
 const tableSchema = new Schema({
     tableNo: { type: String, required: true, unique: true },
     capacity: Number,
-    availability: { 
-        five_six: { type: Boolean, default: true },
-        six_seven: { type: Boolean, default: true }, 
-        seven_eight: { type: Boolean, default: true }, 
-        eight_nine: { type: Boolean, default: true }, 
-        nine_ten: { type: Boolean, default: true }, 
-        ten_eleven: { type: Boolean, default: true }
-    }
+    availability: Object
 });
 
 module.exports = mongoose.model('table', tableSchema, 'Table');
