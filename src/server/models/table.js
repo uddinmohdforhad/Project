f48a9440da4e600b3fd09d8bc952d291a476de9e
HasanAouzoun <hasanozon4@gmail.com>
2019-06-27 02:01:53 +0100

    table BE (get available tables)

--- src/server/models/table.js
+++ src/server/models/table.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const tableSchema = new Schema({
     tableNo: { type: String, required: true, unique: true },
     capacity: Number,
-    availability: Object
+    availability: { type: Object, default: {} }
 });
 
 module.exports = mongoose.model('table', tableSchema, 'Table');
--- src/server/routes/table-routes.js
+++ src/server/routes/table-routes.js
@@ -7,6 +7,21 @@ const Table = require('../models/table')
 router.post("/tables/add", (req, res) => {
   let tableData = req.body
   let table = new Table(tableData)
+  
+  var today = new Date()
+  console.log(today);
+  console.log(today.getFullYear());
+  console.log(today.getMonth());
+  console.log(today.getDate());
+  var todayString = `${today.getFullYear()}${today.getMonth()+1}${today.getDate()}`
+  table.availability[todayString] = { 
+    five_six: true,
+    six_seven: true, 
+    seven_eight: true, 
+    eight_nine: true, 
+    nine_ten: true, 
+    ten_eleven: true
+  }
 
   table.save((error, newTable) => {
     if (error){
@@ -64,6 +79,68 @@ router.get("/tables/getByNumber", (req, res) => {
       res.status(404).send({success: false, message:`no table with number: ${tableNo}`})
     } else {
       res.status(200).send({success: true, table})
+    }
+  })
+})
+
+//Get available tables ==> Given (date) and (time)
+router.get("/tables/getAvailableTables", (req, res) => {
+  let reqBody = req.body
+  let date = reqBody.date
+  let time = reqBody.time
+
+  Table.find((error, tableList) => {
+    if (error) {
+      console.log(error)
+    } else
+    if (!tableList) {
+      res.status(204).send({success: false, message:"Empty list"})
+    } else {
+      var availableTables = []
+
+      tableList.forEach(table => {
+        //check the avail on date
+        var currentAvailability = table.availability[date];
+
+        if(currentAvailability == undefined) {
+          // added the new date to table
+          table.availability[date] = { 
+            five_six: true,
+            six_seven: true, 
+            seven_eight: true, 
+            eight_nine: true, 
+            nine_ten: true, 
+            ten_eleven: true
+          }
+
+          //updated the table
+          Table.findOneAndUpdate({ tableNo: table.tableNo }, table, (error, updatedTable) => {
+            if (error) console.log(error)
+            else if (!updatedTable) console.log("table not found");
+            else console.log("table updated");
+          })
+
+          //add the table to the availableTables array
+          var resTableObj = {
+            tableNo: table.tableNo,
+            capacity: table.capacity
+          }
+          availableTables.push(resTableObj);
+        } else { // if exists
+          var timeAvailTable = currentAvailability[time]
+          if(timeAvailTable == true)
+          {
+            //add the table to the availableTables array
+            var resTableObj = {
+              tableNo: table.tableNo,
+              capacity: table.capacity
+            }
+            availableTables.push(resTableObj);
+          }
+        }
+      })
+
+      res.status(200).send({success: true, availableTables})
     }
   })
 })
