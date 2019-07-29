7908efb9775e6ba45079db8a22b419e81fe86ddd
HasanAouzoun <hasanozon4@gmail.com>
2019-07-29 19:25:00 +0100

    Added extra items on the menu

--- src/app/website/order/order.component.ts
+++ src/app/website/order/order.component.ts
@@ -18,6 +18,15 @@ const MENULIST_DATA: MenuItem[] = [
   {id: '4', name: 'Chicken Tikka Starter', description: "Chicken pieces marinated & barbecued", price: 2.50},
   {id: '5', name: 'Chilli Chicken', description: "Chicken with herbs & spices & fresh chillies", price: 12.50},
   {id: '6', name: 'King Prawn Butterfly', description: "King prawns deep fried & battered", price: 3.50},
+  {id: '7', name: 'Fish Pakora', description: "Spiced fish deep fried with butter", price: 3.50},
+  {id: '8', name: 'Prawn Puri', description: "Deep fired bread with prawn stuffing", price: 3.40},
+  {id: '9', name: 'Fish Tikka', description: "Spacied fish cooked in tanduri oven", price: 5.80},
+  {id: '10', name: 'Prawn Cocktail', description: "Prawns in a cocktail souce", price: 3.50},
+  {id: '11', name: 'Onion Bahja', description: "Deep fried spaced onion balls", price: 3.60},
+  {id: '12', name: 'Mixed Kebbab', description: "Chicken and lamb meet on gril", price: 7.50},
+  {id: '13', name: 'Lamb Tikka', description: "Lamb marinated and barbequed", price: 5.50},
+  {id: '14', name: 'Shish Kebbab', description: "Minced meet tanduried", price: 7.50},
+  {id: '15', name: 'Meet Sambousak', description: "Deep fried meet pasties", price: 4.80},
 ];
 
 @Component({
